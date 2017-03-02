6842c5ff97f04ca07dab78cc8ac72991d1dc8bf8
Trygve Bertelsen Wiig <trygvebw@gmail.com>
2017-03-02 19:43:39 +0100

    Tekst i sirkler

--- static/graph.js
+++ static/graph.js
@@ -1,21 +1,18 @@
+let width = 800;
+let height = 500;
 let nodes = [
-	{x: 0.1, y: 0.1},
-	{x: 0.2, y: 0.2},
-	{x: 0.1, y: 0.8}
+	{x: 0.1*width, y: 0.1*height, name: 'Julius'},
+	{x: 0.2*width, y: 0.2*height, name: 'Fulvia'},
+	{x: 0.1*width, y: 0.8*height, name: 'Octavia'}
 ]
 let edges = [
 	{source: 0, target: 1},
 	{source: 0, target: 2}
 ]
-let width = 800;
-let height = 500;
-
-let nodesAbsoluteLocations = nodes.map(el => ({x: el.x*width, y: el.y*height}))
-console.log(nodesAbsoluteLocations)
 
 let force = d3.layout.force()
 	.size([width, height])
-	.nodes(nodesAbsoluteLocations)
+	.nodes(nodes)
 	.links(edges)
 	.linkDistance(width/2)
 	.linkStrength(0.2)
@@ -33,18 +30,28 @@ let edge = svg.selectAll('.link')
 	.attr('class', 'link')
 
 let node = svg.selectAll('.node')
-	.data(nodesAbsoluteLocations)
+	.data(nodes)
 	.enter()
-	.append('circle')
+	.append('g')
+
+let circle = node.append('circle')
 	.attr('class', 'node')
 	.style('stroke', d => 'red')
 	.style('stroke-width', d => width/250)
 	.style('fill', d => 'white')
 
+let text = node.append('text')
+	.attr('class', 'node-label')
+	.text(d => d.name)
+
 force.on('tick', () => {
-	node.attr('r', width/20)
+	circle.attr('r', width/20)
 		.attr('cx', d => d.x)
 		.attr('cy', d => d.y)
+
+	text.attr('x', d => d.x - width/40)
+		.attr('y', d => d.y + width/120)
+		.style('fill', 'black')
 
 	edge.attr('x1', d => d.source.x)
 		.attr('y1', d => d.source.y)
